b34975ee4ab2ef4185d0075543dbce9d6fb2c56d
spacemile <ansanghyun20.apple@gmail.com>
2023-07-30 18:52:07 +0900

    fix source

--- src/main/java/com/shan/producer/ProducerApplication.java
+++ src/main/java/com/shan/producer/ProducerApplication.java
@@ -20,23 +20,27 @@ public class ProducerApplication {
 
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+				"org.apache.kafka.common.serialization.StringSerializer");
 		props.put(ProducerConfig.ACKS_CONFIG, "all");
 		props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
 		props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+		
 
 		Producer<String, String> producer = new KafkaProducer<>(props);
-		ProducerRecord<String, String> record = new ProducerRecord<>("TEST.TOPIC.1", null, "Hello World");
-		Future<RecordMetadata> future = producer.send(record); // 전송 요청
-
-		try {
-			RecordMetadata metadata = future.get();
-			System.out.println("topic:    " + metadata.topic());
-			System.out.println("partition : " + metadata.partition());
-			System.out.println("offset    : " + metadata.offset());
-			System.out.println("timestamp : " + metadata.timestamp());
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
+		for (int i = 0; i < 10; i++) {
+			ProducerRecord<String, String> record = new ProducerRecord<>("TEST.TOPIC.1", null, Integer.toString(i));
+			Future<RecordMetadata> future = producer.send(record); // 전송 요청
+			try {
+				RecordMetadata metadata = future.get();
+				System.out.println("topic:    " + metadata.topic());
+				System.out.println("partition : " + metadata.partition());
+				System.out.println("offset    : " + metadata.offset());
+				System.out.println("timestamp : " + metadata.timestamp());
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
+
 	}
 }
