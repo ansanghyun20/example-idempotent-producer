34bd0b8e35d1a2e7780ed23e542167da24c76a30
spacemile <ansanghyun20.apple@gmail.com>
2023-07-30 17:57:23 +0900

    init proj

---
+++ src/main/java/com/shan/producer/ProducerApplication.java
@@ -0,0 +1,42 @@
+package com.shan.producer;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.kafka.clients.producer.*;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	// 멱등성 프로듀서
+	// idempotent producer
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+
+		HashMap<String, Object> props = new HashMap<>();
+
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+		props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+		Producer<String, String> producer = new KafkaProducer<>(props);
+		ProducerRecord<String, String> record = new ProducerRecord<>("TEST.TOPIC.1", null, "Hello World");
+		Future<RecordMetadata> future = producer.send(record); // 전송 요청
+
+		try {
+			RecordMetadata metadata = future.get();
+			System.out.println("topic:    " + metadata.topic());
+			System.out.println("partition : " + metadata.partition());
+			System.out.println("offset    : " + metadata.offset());
+			System.out.println("timestamp : " + metadata.timestamp());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+}
